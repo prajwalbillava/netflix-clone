73a69ce69db35818501e061c18ed964bf2ac695a
Prajwal Poojary <prajwalgpoojary999@gmail.com>
2023-08-05 00:13:36 +0530

    commit 2

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "./axios";
+import axios from "axios";
 import "./Row.css";
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
